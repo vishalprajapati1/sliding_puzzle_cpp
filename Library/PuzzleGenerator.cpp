f31b3a84c9de390d2d017731bae3226cc98915bc
vishal <79570443+vishalprajapati1@users.noreply.github.com>
2022-12-25 11:06:18 +0530

    integrated cmake and added github actions

---
+++ Library/PuzzleGenerator.cpp
@@ -0,0 +1,42 @@
+#include <unordered_set>
+#include <queue>
+#include <array>
+#include <iostream>
+#include "puzzleState.h"
+#include "puzzleGenerator.h"
+
+// namespace Puzzle {
+void Puzzle::generateAndPrint(int size, int moves_to_solve) {
+  // left 0 right 1 up 2 down 3
+  const std::array<int, 4> k_directions = {0, 1, 2, 3};
+
+  PuzzleState solved_puzzle(size);
+  int depth = -1;
+  std::queue<PuzzleState> q;
+  std::unordered_set<PuzzleState, PuzzleState::custom_hash> visited;
+  q.push(solved_puzzle);
+  visited.insert(solved_puzzle);
+
+  
+  PuzzleState current_state = solved_puzzle;
+  while (!q.empty() && depth < moves_to_solve) {
+    int states_in_level = q.size();
+    while (states_in_level-- > 0) {
+      current_state = q.front();
+      q.pop();
+      for (auto direction : k_directions) {
+        if (current_state.validMove(direction)) {
+          PuzzleState new_state = current_state.move(direction);
+          if (visited.find(new_state) == visited.end()) {
+            visited.insert(new_state);
+            q.push(new_state);
+          }
+        }
+      }
+    }
+    ++depth;
+  }
+  current_state.print();
+  std::cout << "\nSolveable in " << depth << " moves.";
+}
+// };
---
+++ Library/PuzzleSolver.cpp
@@ -0,0 +1,64 @@
+#include "puzzleState.h"
+#include "puzzleSolver.h"
+#include <array>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+
+// namespace Puzzle {
+void Puzzle::solveAndPrint(PuzzleState unsolved) {
+  // left 0 right 1 up 2 down 3
+  const std::array<int, 4> k_directions = {0, 1, 2, 3};
+
+  PuzzleState solved = PuzzleState(unsolved.size());
+  std::queue<PuzzleState> q;
+  // puzzle ,  parent of puzzle
+  std::unordered_map<PuzzleState, PuzzleState, PuzzleState::custom_hash>
+      visited;
+  q.push(unsolved);
+  visited.insert({unsolved, unsolved});
+  bool isSolved = false;
+
+  while (!q.empty()) {
+    PuzzleState current_state = q.front();
+    q.pop();
+    if (current_state == solved) {
+      isSolved = true;
+      break;
+    }
+
+    for (auto direction : k_directions) {
+      if (current_state.validMove(direction)) {
+        PuzzleState new_state = current_state.move(direction);
+        if (visited.find(new_state) == visited.end()) {
+          visited.insert({new_state, current_state});
+          q.push(new_state);
+        }
+      }
+    }
+  }
+  if (!isSolved) {
+    std::cout << "\n\nUnsolveable\n\n";
+    return;
+  }
+
+  // backtrack
+  std::vector<PuzzleState> steps;
+  PuzzleState current_state = solved;
+  while (!(current_state == unsolved)) {
+    steps.push_back(current_state);
+    auto it = visited.find(current_state);
+    if (it == visited.end()) break;
+    current_state = it->second;
+  }
+  steps.push_back(unsolved);
+  std::reverse(steps.begin(), steps.end());
+
+  int step = 0;
+  for (auto state : steps) {
+    std::cout << "Step # " << (step++);
+    state.print();
+  }
+}
+// };  // namespace Puzzle
